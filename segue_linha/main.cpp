0b0ea3c5f6bfb2243b7f6580ec1bd49bc4c877b6
Francisco Pedro <66444231+franc-pedro@users.noreply.github.com>
2022-11-11 15:39:19 -0100

    Add files via upload

---
+++ segue_linha/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include "ESP32MotorControl.h"
+
+#define LEFT 34
+#define CENTER 35
+#define RIGHT 32
+
+
+void setup()
+{
+  MotorControl.attachMotors(17, 18, 26, 27);
+  Serial.begin(115200);
+  pinMode(LEFT, INPUT);
+  pinMode(CENTER, INPUT);
+  pinMode(RIGHT, INPUT);  
+}
+
+void loop(){
+
+      if (digitalRead(LEFT) == 0 && digitalRead(RIGHT) == 0 && digitalRead(CENTER) == 1)
+      {
+        MotorControl.motorForward(0, 50);
+        MotorControl.motorForward(1, 50);
+      }
+      else if (digitalRead(LEFT) == 1)
+      {
+        MotorControl.motorForward(1, 55);
+        MotorControl.motorReverse(0, 20);
+      }
+      else if (digitalRead(RIGHT) == 1)
+      {
+        MotorControl.motorReverse(1, 20);
+        MotorControl.motorForward(0, 55);
+      }
+      else
+      {
+        MotorControl.motorForward(0, 50);
+        MotorControl.motorForward(1, 50);
+      }
+}
